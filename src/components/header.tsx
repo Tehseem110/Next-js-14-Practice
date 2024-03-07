4212f721395d8dabdebab53cc969933ce3a38aa3
Tehseem110 <Tehseem110>
2024-03-07 15:22:54 +0530

    styled navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 export default function Header() {
   return (
-    <div className="w-full p-2 flex justify-between">
-      <Link href="/">Home</Link>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">reliability</Link>
-      <Link href="/scale">scale</Link>
+    <div className="w-full absolute text-white z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href="/">Home</Link>
+        <div className="flex  gap-2">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">reliability</Link>
+          <Link href="/scale">scale</Link>
+        </div>
+      </nav>
     </div>
   );
 }
